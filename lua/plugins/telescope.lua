ed567492aa2c7b5052975ce9b87bdf8562956a50
minukchoi <6304230+minukchoi@users.noreply.github.com>
2024-01-24 06:08:17 +0000

    adding telescope live grep with args

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,45 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  dependencies = {
+    {
+      "nvim-telescope/telescope-live-grep-args.nvim",
+      -- This will not install any breaking changes.
+      -- For major updates, this must be adjusted manually.
+      version = "^1.0.0",
+    },
+  },
+  config = function()
+    -- require("telescope").load_extension("live_grep_args")
+
+    local telescope = require("telescope")
+    local lga_actions = require("telescope-live-grep-args.actions")
+
+    telescope.setup({
+      extensions = {
+        live_grep_args = {
+          auto_quoting = true, -- enable/disable auto-quoting
+          -- define mappings, e.g.
+          mappings = { -- extend mappings
+            i = {
+              ["<C-k>"] = lga_actions.quote_prompt(),
+              ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+            },
+          },
+          -- ... also accepts theme settings, for example:
+          -- theme = "dropdown", -- use dropdown theme
+          -- theme = { }, -- use own theme spec
+          -- layout_config = { mirror=true }, -- mirror preview pane
+        },
+      },
+    })
+
+    telescope.load_extension("live_grep_args")
+  end,
+  keys = {
+    {
+      "<leader>/",
+      ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>",
+      desc = "Grep (live grep with args)",
+    },
+  },
+}
