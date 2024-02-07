c4802ccceafeeff2b03128a18657685efba53a86
minukchoi <6304230+minukchoi@users.noreply.github.com>
2024-02-07 14:38:25 -0500

    update color for selected code

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -5,12 +5,35 @@ return {
     priority = 1000,
     opts = {
       transparent_background = true,
+      color_overrides = {
+        all = {
+          text = "#ffffff",
+          surface1 = "#817268",
+          -- surface1 = "#689d6a",
+        },
+        latte = {
+          base = "#ff0000",
+          mantle = "#242424",
+          crust = "#474747",
+        },
+        frappe = {},
+        macchiato = {},
+        mocha = {},
+      },
+      custom_highlights = function(colors)
+        return {
+          Comment = { fg = colors.green },
+          TabLineSel = { bg = colors.blue },
+          CmpBorder = { fg = colors.surface2 },
+          Pmenu = { bg = colors.none },
+        }
+      end,
     },
   },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin",
+      colorscheme = "catppuccin-frappe",
     },
   },
 }
